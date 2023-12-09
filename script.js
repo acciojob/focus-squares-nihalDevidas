dab411a7f8d3109b5609463aa791bf6e873668d4
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-12-09 16:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+const squareCon = document.getElementsByClassName("square");
+let arr = Array.from(squareCon);
+console.log(arr)
+arr.forEach((box)=>{
+	box.addEventListener("mouseenter", (e)=>setChanges(e))
+	box.addEventListener("mouseleave", restoreChanges)
+})
+
+
+
+function setChanges(e){
+
+	const id = e.target.id; // id of element that triggerd the event
+	
+	arr.forEach((box)=>{
+		if(box.id === id){
+			box.style.backgroundColor = "#E6E6FA" ;
+		}
+		else{
+			box.style.backgroundColor = "#6F4E37";
+		}
+	});
+}
+
+function restoreChanges(){
+	arr.forEach((box)=> box.style.backgroundColor = "#E6E6FA")
+}
